21ea10426e86f150cadfd5325ff4ec0d29510cf1
Mike Solomon <mike@mikesolomon.org>
2016-10-17 12:20:41 +0300

    Adds full-screen pink for controllers

---
+++ public/javascripts/app.js
@@ -0,0 +1,23 @@
+// in the top-level module of the app
+angular.module('redballoon', [
+  'btford.socket-io'
+]).
+factory('socketToMe', function (socketFactory) {
+  return socketFactory();
+}).
+controller('MainCtrl', function ($scope, socketToMe) {
+
+  $scope.clicked = false;
+
+  socketToMe.on('delta', function () {
+    $scope.clicked = true;
+    $scope.$apply();
+    setTimeout(function(){window.location.reload();},50);
+
+  });
+
+  $scope.setstate = function(st) {
+    $scope.clicked = true;
+    socketToMe.emit('setstate', st);
+  }
+});
