06583c0ead5ba80e9d0b87d479fa61f20aaa1db1
Mike Solomon <mike@mikesolomon.org>
2016-10-17 13:03:52 +0300

    Makes controller more modular

--- routes/controller.js
+++ routes/controller.js
@@ -1,21 +1,27 @@
-// @flow
+// @flow weak
 var express = require('express');
 var state = require('./../state/state');
-var router = express.Router();
 var tops = require('./../consts/scenes');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  state.get().then(function(st) {
-    if (st.sc == null) {
-      res.render('loading');
-    } else {
-      res.render('controllerprod', {
-        now: st.top,
-        nowText: tops[st.top],
-        states: require('./../state/states')
-      });
-    }
+
+var proddev = function(prod) {
+  var router = express.Router();
+  /* GET home page. */
+  return router.get('/', function(req, res, next) {
+    state.get().then(function(st) {
+      if (st.sc == null) {
+        console.log('loading main page');
+        res.render('loading');
+      } else {
+        console.log('rendering controller page ' + st.top);
+        res.render(prod ? 'controllerprod' : 'controllerdev', {
+          now: st.top,
+          nowText: tops[st.top],
+          states: require('./../state/states'),
+          nav: require('./../nav/nav')(st)
+        });
+      }
+    });
   });
-});
-module.exports = router
+}
+module.exports = proddev
