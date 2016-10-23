ff460c7568b7eb576b014dcf2cea372d505b5006
Mike Solomon <mike@mikesolomon.org>
2016-10-23 15:53:27 +0300

    Adds the moment of letting go

--- app/tops/letgo.js
+++ app/tops/letgo.js
@@ -6,21 +6,22 @@ var ril = Sequitur.rerouteIfLate('sc', 'ignore');
 var EventEmitter = require('events').EventEmitter;
 var _ = require('lodash');
 var common = require('./common');
-var carrefour = {
-  'carrefour': s.nextBuffer(),
+var letgo = {
+  'letgo': s.nextBuffer(),
 }
-var carrefourId = s.nextNodeID()
+var letgoId = s.nextNodeID()
 var event = new EventEmitter();
 var scene = new Sequitur(event);
-scene.at('0.0s', ril, ["/s_new", "carrefourPlayer", carrefourId, 0, common.group, "out", 0, "bufnum", carrefour.carrefour]);
+
+scene.at('0.0s', ril, ["/s_new", "letgoPlayer", letgoId, 0, common.group, "out", 0, "bufnum", letgo.letgo]);
 
 module.exports = {
-  carrefourId: carrefourId,
-  buffers: carrefour,
+  letgoId: letgoId,
+  buffers: letgo,
   scene: scene,
   event: event,
   // for now same as swing player
   synthdefs: [
-  'SynthDef.new("carrefourPlayer",{|out, bufnum, mul = 1.0, gate = 1|Out.ar(out, EnvGen.kr(Env.asr(0.01,1,6,-1),gate,doneAction:2) * PlayBuf.ar(2,bufnum,doneAction:2) * mul);})'
+  'SynthDef.new("letgoPlayer",{|out, bufnum, mul = 1.0|Out.ar(out, PlayBuf.ar(2,bufnum,doneAction:2) * mul);})'
 ]
 }
