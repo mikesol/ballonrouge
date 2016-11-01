568e82d409688f32203b50d25da66600b58f5c21
Mike Solomon <mike@mikesolomon.org>
2016-11-01 07:21:27 +0100

    Adds new files

--- app/tops/tops.js
+++ app/tops/tops.js
@@ -11,8 +11,10 @@ var city1 = require('./city1')
 var letgo = require('./letgo')
 var windows = require('./windows')
 var ride = require('./ride')
+var city1end = require('./city1end')
+var cross = require('./cross')
 
 module.exports = [
   entry, entryend, swing, swingend, carrefour, carrefourend, grab, city1, letgo, windows, ride,
-  {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+  city1end, cross, {}, {}, {}, {}, {}, {}, {}, {}, {}
 ];
