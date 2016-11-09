853f4b9264784ae987962ebe2cca9cca763ef4a9
Mike Solomon <mike@mikesolomon.org>
2016-11-09 23:47:59 +0100

    Fixes cross end

--- app/tops/crossend.js
+++ app/tops/crossend.js
@@ -9,16 +9,10 @@ var cross = require('./cross');
 var common = require('./common');
 var event = new EventEmitter();
 var scene = new EvSeq(event);
-var crossend = {
-  'crossend': s.nextBuffer(),
-}
-
-var buffers = _.assign({}, cross);
 
 scene.at('0.0s', ril, ["/n_set", cross.crossId, 'gate', 0]);
 
 module.exports = {
   scene: scene,
-  event: event,
-  buffers: crossend
+  event: event
 }
