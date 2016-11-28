b489672f6cab9484dca5b7747d6a9625ec412b29
Mike Solomon <mike@mikesolomon.org>
2016-11-28 08:58:13 +0200

    Adds stop

--- app/consts/scenes.js
+++ app/consts/scenes.js
@@ -14,5 +14,6 @@ module.exports = [
   'tempête',
   'jardin de lumières',
   'vole',
+  'fin vole',
   'saluons!'
 ];
--- app/tops/exit.js
+++ app/tops/exit.js
@@ -5,14 +5,10 @@ var s = require('./../supercollider/next');
 var ril = EvSeq.rerouteIfLate('sc', 'ignore');
 var EventEmitter = require('events').EventEmitter;
 var _ = require('lodash');
-var final = require('./final');
 var common = require('./common');
 var aiftools = require('./../aiftools/aiftools');
 var event = new EventEmitter();
 var scene = new EvSeq(event);
-
-// can also use carrefourPlayer
-scene.at('0.0s', ril, ["/n_free", final.finalGateId], 'finalEnd');
 
 var mpqq = {
   'mpqq_15_melody': s.nextBuffer()
